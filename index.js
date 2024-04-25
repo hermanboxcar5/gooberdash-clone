60ec4df682f6b22cc28680540419a2248231f9c4
hermanboxcar5 <95950881+hermanboxcar5@users.noreply.github.com>
2024-04-25 16:10:57 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,18 +2,11 @@ const express = require("express")
 const https = require("https")
 const app = express()
 
-app.get("/chess/*", (req, res, next) => {
-  const pathSegments = req.path.split('/');
-  const discordPath = pathSegments[2]
-  https.request(new URL("https://chess.com/" + discordPath), (resp) => {
-    res.contentType(resp.headers["content-type"])
-    resp.pipe(res)
-  }).end()
-})
+
 app.get("/*", (req, res, next) => {
   //const pathSegments = req.path.split('/');
  // const rocketPath = pathSegments[2]
-  https.request(new URL("https://rocketbotroyale.winterpixel.io/" + req.path), (resp) => {
+  https.request(new URL("https://gooberdash.winterpixel.io/" + req.path), (resp) => {
     res.contentType(resp.headers["content-type"])
     resp.pipe(res)
   }).end()
